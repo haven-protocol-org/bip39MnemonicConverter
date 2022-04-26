f27d58afbb66831674779e5660348f3605c04074
Marty <51510876+MartyHav@users.noreply.github.com>
2022-04-26 17:32:21 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,1 @@
-export {convertBip39ToHavenMnemonic convertBip39ToMoneroMnemonic} from "./converter/src/mnemonicConverter"
+export {convertBip39ToHavenMnemonic, convertBip39ToMoneroMnemonic} from "./converter/src/mnemonicConverter"
