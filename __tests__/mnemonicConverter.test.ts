ad40a1a08fee9c58587b0a6817fc6d388e7e31f8
Marty <51510876+MartyHav@users.noreply.github.com>
2022-05-18 09:18:28 +0200

    Update mnemonicConverter.test.ts

--- __tests__/mnemonicConverter.test.ts
+++ __tests__/mnemonicConverter.test.ts
@@ -1,22 +1,23 @@
 import { convertBip39ToMoneroMnemonic } from "../converter/src/mnemonicConverter"
+import { convertBip39ToHavenMnemonic } from "../converter/src/mnemonicConverter"
 
 
 describe('test converter', () => {
 
 
-    it("should convert mnemonic", () => {
+    it("should convert to monero mnemonic", () => {
 
         const moneroSeed = convertBip39ToMoneroMnemonic("venture expose swim treat swap defense magic toy blast hover neck permit", "")
-
-
         expect(moneroSeed).toBe("federal honked haystack vampire thirsty sensible talent rustled bias gearbox rims yeti ingested pamphlet yacht anvil tawny akin chrome fazed balding dove imitate inquest tawny")
 
+    })
 
 
+    it("should convert to haven mnemonic", () => {
 
-
-
-
+        const havenSeed = convertBip39ToHavenMnemonic("venture expose swim treat swap defense magic toy blast hover neck permit", "")
+        expect(havenSeed).toBe("juvenile kickoff king glass scoop lair iris token truth puzzled amaze corrode justice autumn pimple turnip cafe oyster hover baffles giddy farming vector western pimple")
+        console.log(havenSeed)
     })
 
 
